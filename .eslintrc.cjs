1c49506f6e7873bd6c5d11ade1153f5aa8a14f20
AbibullaevAR <a.alim2019@mail.ru>
2024-01-24 18:21:37 +0300

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+const path = require('node:path');
+const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    '@vue/eslint-config-airbnb',
+  ],
+  rules: {
+    'import/no-unresolved': 'error',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`,
+    }),
+  },
+};
