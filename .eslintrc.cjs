c8f224dd2e8760a7260eaf0a00e0b25bd890f159
AbibullaevAR <a.alim2019@mail.ru>
2024-01-25 23:47:51 +0300

    remove '@typescript-eslint/recommended'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@typescript-eslint/recommended',
     '@vue/eslint-config-prettier/skip-formatting',
     '@vue/eslint-config-airbnb',
   ],
