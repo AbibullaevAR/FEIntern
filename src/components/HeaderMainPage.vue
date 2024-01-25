18bb33d02711c16bac4f99939a2fc0020e0f79d0
AbibullaevAR <a.alim2019@mail.ru>
2024-01-25 12:43:30 +0300

    create Main Page Header

---
+++ src/components/HeaderMainPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ThemesChange from './ThemesChange.vue';
+</script>
+
+<template>
+  <header>
+    <img src="@/assets/logo.svg" alt="">
+    <ThemesChange></ThemesChange>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
