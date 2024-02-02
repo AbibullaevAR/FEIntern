6c4cfc60dcd06727f177fb6742c07118ede89001
AbibullaevAR <a.alim2019@mail.ru>
2024-02-02 20:12:13 +0300

    create createdFilter

---
+++ src/components/filters/createdFilter/CreatedFilter.ts
@@ -0,0 +1,16 @@
+import type { TRequestParam } from "@/api/types";
+import AbstractBackendFilter from "../AbstractBackendFilter";
+
+export default class CreatedFilter extends AbstractBackendFilter {
+  fromYear: number | null = null;
+
+  beforeYear: number | null = null;
+
+  getRequestParams(): TRequestParam | TRequestParam[] | null {
+    if (!this.fromYear && !this.beforeYear) return null;
+    if (!this.fromYear) return { key: 'created_lte', value: this.beforeYear as number };
+    if (!this.beforeYear) return { key: 'created_gte', value: this.fromYear as number };
+
+    return [{ key: 'created_lte', value: this.beforeYear }, { key: 'created_gte', value: this.fromYear }];
+  }
+}
---
+++ src/components/filters/createdFilter/CreatedFilter.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import BaseDropdown from '@/components/ui/BaseDropdown.vue';
+import { ref, watch } from 'vue';
+import _ from 'lodash';
+import CreatedFilter from './CreatedFilter';
+
+const fromYear = ref();
+const beforeYear = ref();
+const createdFilter = ref(new CreatedFilter());
+
+watch([beforeYear, fromYear], _.debounce(() => {
+  createdFilter.value.beforeYear = Number(beforeYear.value);
+  createdFilter.value.fromYear = Number(fromYear.value);
+}, 500));
+
+</script>
+
+<template>
+  <div class="created-filter">
+    <BaseDropdown>
+      <template v-slot:trigger>
+        Created
+      </template>
+      <template v-slot:menu>
+        <div class="created-filter__menu">
+          <label><input v-model="fromYear" class="created-filter__entry" type="text" placeholder="from"></label>
+          <span>-</span>
+          <label><input v-model="beforeYear" class="created-filter__entry" type="text" placeholder="before"></label>
+        </div>
+      </template>
+    </BaseDropdown>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.created-filter {
+  &__menu {
+    padding: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__entry {
+    max-width: 110px;
+    background-color: rgb(239, 239, 239);
+    border-radius: 8px;
+    border: none;
+    padding: 13px;
+  }
+  @media screen and (min-width: 768px) {
+    &__menu {
+      flex-direction: column;
+    }
+  }
+  @media screen and (min-width: 1366px) {
+    &__menu {
+      flex-direction: row;
+    }
+  }
+}
+textarea:focus,
+input:focus {
+  outline: none;
+}
+</style>
