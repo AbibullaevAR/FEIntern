999b3457f95ea249ecd20b9057c2f00234312c57
AbibullaevAR <a.alim2019@mail.ru>
2024-02-05 17:33:47 +0300

    create async image component

---
+++ src/components/AsyncImg.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+import getImage from '@/api/image';
+
+const { src } = defineProps({
+  src: { type: String, default: '' },
+});
+
+const imgURL = ref('src/assets/logo.svg');
+
+watchEffect(async () => {
+  const image = await getImage(src);
+
+  if (!image) return;
+
+  imgURL.value = URL.createObjectURL(image);
+});
+</script>
+
+<template>
+  <img :src="imgURL" alt="">
+</template>
